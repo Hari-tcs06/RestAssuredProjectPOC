139c6d3a2843812475c83d7d027a6706fae4907d
harih <harih@LAPTOP-3DPOPGLT>
2022-09-01 16:03:05 +1000

    Take one commit

---
+++ RestAssuredProject_Aug2022/src/test/java/Test01_GET.java
@@ -0,0 +1,37 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+
+public class Test01_GET {
+	
+	@Test
+	
+	void test_01 () 
+	{
+		
+		Response response = get("https://reqres.in/api/users?page=2");
+			
+		System.out.println (response.asString());
+		System.out.println (response.getBody().asString());
+		System.out.println (response.getStatusCode());
+		System.out.println (response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		System.out.println(response.getTime());
+		
+		int statuscode = response.getStatusCode();
+		Assert.assertEquals(statuscode, 201);
+	}
+	
+@Test
+	
+	void test_06 () 
+	{
+	
+	given().get("https://reqres.in/api/users?page=2").then().statusCode(200);
+	
+
+	}
+
+}
