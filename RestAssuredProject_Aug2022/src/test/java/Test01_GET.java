fa62d2fd2f02ff12771996ed04d0d82db4f7d080
harih <harih@LAPTOP-3DPOPGLT>
2022-09-27 16:01:29 +1000

    Allure 27092022

--- RestAssuredProject_Aug2022/src/test/java/Test01_GET.java
+++ RestAssuredProject_Aug2022/src/test/java/Test01_GET.java
@@ -1,12 +1,37 @@
+
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
+//import static utils.extentreports.ExtentTestManager.startTest;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import java.lang.reflect.Method;
+import org.testng.annotations.Listeners;
+
+@Epic("Regression Tests")
+@Feature("Test scenarios 1 and 2")
+
+//import utils.listeners.TestListener;
 
 public class Test01_GET {
 	
-	@Test
+
+	
+	@Test(priority = 0, description = "Test case 1 - Check status code")
+	
+	@Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: Checking status code 1")
+    @Story("Story 1")
 	
 	void test_01 () 
 	{
@@ -21,10 +46,13 @@ public class Test01_GET {
 		System.out.println(response.getTime());
 		
 		int statuscode = response.getStatusCode();
-		Assert.assertEquals(statuscode, 200);
+		AssertJUnit.assertEquals(statuscode, 200);
 	}
 	
-@Test
+@Test (priority = 1, description = "Test case 2 - Check status code")
+@Severity(SeverityLevel.NORMAL)
+@Description("Test Description: Checking status code 2")
+@Story("Story 2")
 	
 	void test_02 () 
 
